e6d89421518f152c46b585c86fa1d8a34d029fb8
JONATHAN <jonpaul_14@hotmail.com>
2024-05-24 15:22:58 -0500

    Login

---
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -0,0 +1,136 @@
+package com.example.login
+
+import android.media.Image
+import android.os.Bundle
+import android.text.style.BackgroundColorSpan
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.login.ui.theme.LoginTheme
+import kotlinx.coroutines.flow.callbackFlow
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LoginTheme {
+
+                }
+            }
+        }
+    }
+
+@Preview
+@Composable
+fun Login(){
+    Column (
+        Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .background(color = Color(android.graphics.Color.parseColor("#ffffff"))),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painterResource(id = R.drawable.top_background),
+            contentDescription = null,
+            contentScale = ContentScale.FillBounds
+        )
+        Image(
+            painterResource(id = R.drawable.logo_cash),
+            contentDescription =null,
+            modifier = Modifier
+                .height(120.dp)
+                .size(300.dp)
+
+        )
+
+        Text("Welcome a CashCraft",
+            color = Color(android.graphics.Color.parseColor("#7d32a8")),
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Italic,
+            fontSize = 30.sp
+
+        )
+        var user by remember { mutableStateOf("Username") }
+        var pass by remember { mutableStateOf("Password") }
+        var passwordVisible by rememberSaveable { mutableStateOf(false) }
+        TextField(value = user,{text ->user = text},
+            Modifier
+                .fillMaxWidth()
+                .height(66.dp)
+                .padding(start = 64.dp, end = 64.dp, top = 8.dp, bottom = 8.dp)
+                .border(
+                    1.dp, color = Color(android.graphics.Color.parseColor("#7d32a8")),
+                    shape = RoundedCornerShape(50)
+                ),
+            shape = RoundedCornerShape(50),
+            textStyle = TextStyle(
+                textAlign = TextAlign.Center,
+                color = Color(android.graphics.Color.parseColor("#7d32a8")),
+                fontSize = 14.sp
+            )
+
+            )
+        TextField(value = pass,{text ->pass = text},
+            Modifier
+                .fillMaxWidth()
+                .height(66.dp)
+                .padding(start = 64.dp, end = 64.dp, top = 8.dp, bottom = 8.dp)
+                .border(
+                    1.dp, color = Color(android.graphics.Color.parseColor("#7d32a8")),
+                    shape = RoundedCornerShape(50)
+                ),
+            shape = RoundedCornerShape(50),
+            textStyle = TextStyle(
+                textAlign = TextAlign.Center,
+                color = Color(android.graphics.Color.parseColor("#7d32a8")),
+                fontSize = 14.sp
+            )
+
+        )
+
+
+
+
+
+    }
+}
+
+
+
